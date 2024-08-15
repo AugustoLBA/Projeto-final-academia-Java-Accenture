530fc256774a434c08a18492ce2a16e042e0ce5c
José Eduardo <dududutragarcia@gmail.com>
2024-08-15 09:21:12 -0300

    Criacao de um data loader

--- src/main/java/br/acc/banco/models/Agencia.java
+++ src/main/java/br/acc/banco/models/Agencia.java
@@ -13,9 +13,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "agencias")
 public class Agencia implements Serializable{
